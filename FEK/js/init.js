65bf6e5ea71a231c9cf676184e0c1de8daa55175
gramosiri2 <52707393+gramosiri2@users.noreply.github.com>
2019-08-20 13:47:34 -0300

    Add files via upload

--- FEK/js/init.js
+++ FEK/js/init.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     $('.carousel').carousel();
     setInterval(function(){
     	$('.carousel').carousel('next');
-    }, 5000)
+    }, 6000)
     $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true});
